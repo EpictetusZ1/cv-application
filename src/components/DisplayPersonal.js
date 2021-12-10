0c6512af8e79fe3cb94428034bb9df84fe1d1a3f
J Heaton <jackrobertheaton@gmail.com>
2021-12-10 12:43:53 -0500

    refactor personal section files, clean index.html

---
+++ src/components/DisplayPersonal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "../styles/DisplayData.module.css"
+
+function DisplayPersonal(props) {
+
+    const formatData = (object) => {
+        const { name, email, phone, city} = object
+        return (
+            <div>
+                <h2>General Information: </h2>
+                <hr className={styles.hr}/>
+                <p> <b>Name: </b> {name}</p>
+                <p> <b>Email: </b> {email}</p>
+                <p> <b>Phone: </b> {phone}</p>
+                <p> <b>City: </b> {city}</p>
+            </div>
+        )
+    }
+
+    const { ...fields } = props.dataObj
+    const btnRef = props.btnRef
+
+    return (
+        <div className={styles.resumeBlock}>
+           <button className={styles.editContent} onClick={btnRef}>Edit</button>
+            {formatData(fields)}
+        </div>
+    )
+}
+
+export default DisplayPersonal
